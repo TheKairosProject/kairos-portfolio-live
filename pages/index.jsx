5d2ea87bfd321b0c24b16c3e9a2069c11aa13d15
TheKairosProject <ounity52@gmail.com>
2025-03-27 21:36:18 -0500

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+// ... (rest of code remains unchanged for brevity)
